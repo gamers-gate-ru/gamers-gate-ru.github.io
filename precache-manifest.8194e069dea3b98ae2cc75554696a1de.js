286562875f102735ec7ca7c3e58548f83cbe358c
Юрий Гринёв <a1d516ac5f5d290@gmail.com>
2020-10-05 22:46:57 +0500

    deploy

---
+++ precache-manifest.8194e069dea3b98ae2cc75554696a1de.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaac5b204fe3337fdc9",
+    "url": "/css/app.16e866a8.css"
+  },
+  {
+    "revision": "709424c13729f04899f4f56663878785",
+    "url": "/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "af7fd320fbc52f767f539c7c52f89c3e",
+    "url": "/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "bfdd980a6a36ef5ed9d1f305d646fa32",
+    "url": "/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "e4e40b0c82d228add33b5bcfe276a859",
+    "url": "/icons/browserconfig.xml"
+  },
+  {
+    "revision": "49aa7966f92b191e113b340d035c5da1",
+    "url": "/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "6764870974e722d322bfdc1c3fd9d999",
+    "url": "/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "3cab026cf6093c79a4665aa1d76f07e2",
+    "url": "/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8aeaa329a55c9f4b47ea08e5f30ce693",
+    "url": "/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/icons/site.webmanifest"
+  },
+  {
+    "revision": "c47147280d5cfe87f8e292c53dbe5ded",
+    "url": "/img/aleksey-pilipchuk.c4714728.jpg"
+  },
+  {
+    "revision": "13aa95718ea625aa7687795c7abad636",
+    "url": "/img/anton-logvinov.13aa9571.jpg"
+  },
+  {
+    "revision": "d1115fbe83ec8a8b18140b083df35efd",
+    "url": "/img/cover.d1115fbe.png"
+  },
+  {
+    "revision": "3e1b3143695c1fa5f66a20a8e972e5ba",
+    "url": "/img/denis-knyazev.3e1b3143.jpg"
+  },
+  {
+    "revision": "47c041c6fabb76ff350f905d1d7123fb",
+    "url": "/img/dmitry-kungurov.47c041c6.jpg"
+  },
+  {
+    "revision": "c56c0dd2869f5bf3017fa1bf0c1d581b",
+    "url": "/img/itdx.c56c0dd2.jpg"
+  },
+  {
+    "revision": "c47235c82f8a818c0a1012692b670e04",
+    "url": "/img/no-image.c47235c8.jpg"
+  },
+  {
+    "revision": "2274a5fe61fcbad9c96c1bf4099deba3",
+    "url": "/img/vadim-elistratov.2274a5fe.jpg"
+  },
+  {
+    "revision": "d3f38360e1762001cc1015a1e54da969",
+    "url": "/img/valentin-petukhov.d3f38360.jpg"
+  },
+  {
+    "revision": "3133f75dfbcdd08ba34bca7705daac39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbaac5b204fe3337fdc9",
+    "url": "/js/app.2265f0cc.js"
+  },
+  {
+    "revision": "cb2177f86359096ac704",
+    "url": "/js/chunk-vendors.4315561d.js"
+  },
+  {
+    "revision": "fbb353ca473b2f01172255f832efaeff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
