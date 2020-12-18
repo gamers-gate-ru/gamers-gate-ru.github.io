f35c9330fa3cea2d751e064c119887b5266b4e6c
Юрий Гринёв <a1d516ac5f5d290@gmail.com>
2020-12-18 18:08:01 +0500

    deploy

---
+++ precache-manifest.9601a2c698151c091715a11f9649a23f.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7500951f8d42b62ddd",
+    "url": "/css/app.d8409ded.css"
+  },
+  {
+    "revision": "709424c13729f04899f4f56663878785",
+    "url": "/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "af7fd320fbc52f767f539c7c52f89c3e",
+    "url": "/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "bfdd980a6a36ef5ed9d1f305d646fa32",
+    "url": "/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "e4e40b0c82d228add33b5bcfe276a859",
+    "url": "/icons/browserconfig.xml"
+  },
+  {
+    "revision": "49aa7966f92b191e113b340d035c5da1",
+    "url": "/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "6764870974e722d322bfdc1c3fd9d999",
+    "url": "/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "3cab026cf6093c79a4665aa1d76f07e2",
+    "url": "/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8aeaa329a55c9f4b47ea08e5f30ce693",
+    "url": "/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/icons/site.webmanifest"
+  },
+  {
+    "revision": "d1115fbe83ec8a8b18140b083df35efd",
+    "url": "/img/419a8903-728d-435d-a1dd-e41538408528.d1115fbe.png"
+  },
+  {
+    "revision": "d6283071c8cb8b31b46a9151067dfcce",
+    "url": "/img/5a837ed7-f649-44da-9df9-ba912bd20a77.d6283071.png"
+  },
+  {
+    "revision": "5bc560652902c89d9cf3565534f0cf80",
+    "url": "/img/aleksej-egorov.5bc56065.jpg"
+  },
+  {
+    "revision": "bdf1cb5f5095f3f04d7b59ffbacbcf22",
+    "url": "/img/aleksej-lihachev.bdf1cb5f.jpg"
+  },
+  {
+    "revision": "c47147280d5cfe87f8e292c53dbe5ded",
+    "url": "/img/aleksey-pilipchuk.c4714728.jpg"
+  },
+  {
+    "revision": "13aa95718ea625aa7687795c7abad636",
+    "url": "/img/anton-logvinov.13aa9571.jpg"
+  },
+  {
+    "revision": "6f0feac85cc96806ca06edc24a80568f",
+    "url": "/img/daniil-kortez.6f0feac8.jpg"
+  },
+  {
+    "revision": "3e1b3143695c1fa5f66a20a8e972e5ba",
+    "url": "/img/denis-knyazev.3e1b3143.jpg"
+  },
+  {
+    "revision": "0e09cfd09ec28354a3a31d9530043bd7",
+    "url": "/img/denis-pavlushkin.0e09cfd0.jpg"
+  },
+  {
+    "revision": "47c041c6fabb76ff350f905d1d7123fb",
+    "url": "/img/dmitry-kungurov.47c041c6.jpg"
+  },
+  {
+    "revision": "f31091c6d8c81d1fc03c51b2f758a88f",
+    "url": "/img/dmitry-puchkov.f31091c6.jpg"
+  },
+  {
+    "revision": "8caf5ee54bdac4922c420123572ba29f",
+    "url": "/img/evgenij-peklo.8caf5ee5.jpg"
+  },
+  {
+    "revision": "c56c0dd2869f5bf3017fa1bf0c1d581b",
+    "url": "/img/itdx.c56c0dd2.jpg"
+  },
+  {
+    "revision": "0262418b2c5f44aa93c269a91b37d8ec",
+    "url": "/img/maksim-ivanov.0262418b.jpg"
+  },
+  {
+    "revision": "c47235c82f8a818c0a1012692b670e04",
+    "url": "/img/no-image.c47235c8.jpg"
+  },
+  {
+    "revision": "2274a5fe61fcbad9c96c1bf4099deba3",
+    "url": "/img/vadim-elistratov.2274a5fe.jpg"
+  },
+  {
+    "revision": "d3f38360e1762001cc1015a1e54da969",
+    "url": "/img/valentin-petukhov.d3f38360.jpg"
+  },
+  {
+    "revision": "a2b6ad797ff30e53c302c2502355261f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f7500951f8d42b62ddd",
+    "url": "/js/app.ea449d05.js"
+  },
+  {
+    "revision": "cb2177f86359096ac704",
+    "url": "/js/chunk-vendors.4315561d.js"
+  },
+  {
+    "revision": "fbb353ca473b2f01172255f832efaeff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
